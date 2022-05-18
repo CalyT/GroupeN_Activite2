d91567089d23c21eb503e51f680961ceb2b97204
CalyT <63455669+CalyT@users.noreply.github.com>
2022-05-18 16:45:53 +0200

    testgrille16

--- src/test/java/GrilleImplTest.java
+++ src/test/java/GrilleImplTest.java
@@ -63,32 +63,76 @@ class GrilleImplTest {
     }
 
     @Test
+    public void getValue16x16() {
+        GrilleImpl test = new GrilleImpl(16);
+        assertEquals('0',test.getValue(12,10));
+    }
+
+    @Test
     public void setValue16x16() {
         GrilleImpl test = new GrilleImpl(16);
-        assertEquals('0',test.getValue(9,5));
+        assertEquals('@',test.getValue(12,10);
+        test.setValue(9,5, "7");
+        assertEquals('7',test.getValue(12,10));
     }
 
     @Test
     public void possible16x16() {
         GrilleImpl test = new GrilleImpl(16);
-        assertEquals(false,test.possible(9,5, 'y'));
+        test.setValue(0,0, "e");
+        test.setValue(1,0, "8");
+        test.setValue(2,0, "a");
+        test.setValue(3,0, "b");
+        test.setValue(4,0, "0");
+        test.setValue(5,0, "1");
+        test.setValue(6,0, "2");
+        test.setValue(7,0, "5");
+        test.setValue(8,0, "4");
+        test.setValue(9,0, "f");
+        test.setValue(10,0, "3");
+        test.setValue(11,0, "6");
+        test.setValue(12,0, "d");
+        test.setValue(13,0, "7");
+        test.setValue(14,0, "c");
+        test.setValue(15,0, "9");
+
+        test.setValue(0,1, "7");
+        test.setValue(0,2, "5");
+        test.setValue(0,3, "2");
+        test.setValue(0,4, "f");
+        test.setValue(0,5, "8");
+        test.setValue(0,6, "a");
+        test.setValue(0,7, "b");
+        test.setValue(0,8, "d");
+        test.setValue(0,9, "1");
+        test.setValue(0,10, "3");
+        test.setValue(0,11, "0");
+        test.setValue(0,12, "c");
+        test.setValue(0,13, "4");
+        test.setValue(0,14, "9");
+        test.setValue(0,15, "6");
+
+        assertEquals(false,test.possible(12,10, '5'));
+        assertEquals(false,test.possible(12,10, '0'));
+
+        assertEquals(true,test.possible(12,10, '7'));
     }
 
     @Test
     public void getDimension16x16() {
         GrilleImpl test = new GrilleImpl(16);
-        assertEquals('0',test.getValue(9,5));
-    }
-
-    @Test
-    public void getValue16x16() {
-        GrilleImpl test = new GrilleImpl(16);
-        assertEquals('0',test.getValue(9,5));
+        assertEquals('0',test.getDimension(12,10, "4", "12"));
     }
 
     @Test
     public void complete16x16() {
         GrilleImpl test = new GrilleImpl(16);
-        assertEquals('0',test.getValue(9,5));
+        assertEquals(false,test.complete());
+    }
+
+    @Test
+    public void constructeur16x16() {
+        GrilleImpl test = new GrilleImpl(16);
+        assertEquals(false,test.constructeur());
     }
 }
