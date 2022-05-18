2a2e1bd11a21e41b7a5cc45f756986fbccc06347
Fenouil <63717793+LeFenouil@users.noreply.github.com>
2022-05-18 15:11:15 +0200

    testàfaire

--- src/main/java/GrilleImpl.java
+++ src/main/java/GrilleImpl.java
@@ -1,70 +1,48 @@
 /**
- * Interface d'une grille de sudoku
+ * Implementation d'une grille
  */
-public interface GrilleImpl {
-    /**
-     * Caractere de case vide
-     */
-    static final char EMPTY = '@';
+public class GrilleImpl implements Grille {
 
     /**
-     * Caractere possible a mettre dans la grille
-     *
-     * pour une grille 9x9 : 1..9
-     *
-     * pour une grille 16x16: 0..9-a..f
+     * Variable de dimension
      */
-    static final char[] possible = new char[] { '1', '2', '3', '4', '5', '6',
-            '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f' };
+    private int dim;
 
     /**
-     * @return largeur/hauteur de la grille
+     * Variable de dimension
      */
-    public int getDimension();
+    private char[][] grille;
 
     /**
-     * Affecte une valeur dans la grille
-     *
-     * @param x       position x dans la grille
-     * @param y       position y dans la grille
-     * @param value
-     *            valeur a mettre dans la case
-     * @throw IllegalArgumentException si x ou y sont hors bornes (0-8)
-     * @throw IllegalArgumentException si la valeur est interdite aux vues des
-     *        autres valeurs de la grille
-     * @throw IllegalArgumentException si value n'est pas un caractere autorise
-     *        ('1',...,'9')
+     * Constructeur
      */
-    public void setValue(int x, int y, char value) throws IllegalArgumentException;
+    public GrilleImpl(int dim) {
+        this.dim = dim;
+        this.grille = new char[dim][dim];
+    }
 
-    /**
-     * Recupere une valeur de la grille
-     *
-     * @param x      position x dans la grille
-     * @param y      position y dans la grille
-     * @return valeur dans la case x,y
-     * @throw IllegalArgumentException si x ou y sont hors bornes (0-8)
-     */
-    public char getValue(int x, int y) throws IllegalArgumentException;
+    @Override
+    public int getDimension() {
+        return 0;
+    }
 
-    /**
-     * Test si une grille est terminee
-     *
-     * @return true si la grille est complete
-     */
-    public boolean complete();
+    @Override
+    public void setValue(int x, int y, char value) throws IllegalArgumentException {
 
-    /**
-     * Test si une valeur est possible dans la grille par rapport a ce qu'elle
-     * contient deja
-     *
-     * @param x     position x dans la grille
-     * @param y     position y dans la grille
-     * @param value
-     *            valeur a mettre dans la case
-     * @throw IllegalArgumentException si x ou y sont hors bornes (0-8)
-     * @throw IllegalArgumentException si value n'est pas un caractere autorise
-     *        ('1',...,'9',..)
-     */
-    public boolean possible(int x, int y, char value) throws IllegalArgumentException;
+    }
+
+    @Override
+    public char getValue(int x, int y) throws IllegalArgumentException {
+        return '0';
+    }
+
+    @Override
+    public boolean complete() {
+        return false;
+    }
+
+    @Override
+    public boolean possible(int x, int y, char value) throws IllegalArgumentException {
+        return false;
+    }
 }
--- src/test/java/GrilleImplTest.java
+++ src/test/java/GrilleImplTest.java
@@ -1,16 +1,66 @@
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+
 
 class GrilleImplTest {
 
     @Test
-    public void testOK() {
-        assertEquals( 4, GrilleImpl.getDimension(12, 16));
-
+    public void setValue9x9() {
+        GrilleImpl test = new GrilleImpl(9);
+        assertEquals('0',test.getValue(9,5));
     }
 
-    //test change
+    @Test
+    public void possible9x9() {
+        GrilleImpl test = new GrilleImpl(9);
+        assertEquals(false,test.possible(9,5, 'y'));
+    }
+
+    @Test
+    public void getDimension9x9() {
+        GrilleImpl test = new GrilleImpl(9);
+        assertEquals('0',test.getDimension(9,5));
+    }
+
+    @Test
+    public void getValue9x9() {
+        GrilleImpl test = new GrilleImpl(9);
+        assertEquals('0',test.getValue(9,5));
+    }
+
+    @Test
+    public void complete9x9() {
+        GrilleImpl test = new GrilleImpl(9);
+        assertEquals('0',test.getValue(9,5));
+    }
+
+    @Test
+    public void setValue16x16() {
+        GrilleImpl test = new GrilleImpl(16);
+        assertEquals('0',test.getValue(9,5));
+    }
+
+    @Test
+    public void possible16x16() {
+        GrilleImpl test = new GrilleImpl(16);
+        assertEquals(false,test.possible(9,5, 'y'));
+    }
+
+    @Test
+    public void getDimension16x16() {
+        GrilleImpl test = new GrilleImpl(16);
+        assertEquals('0',test.getValue(9,5));
+    }
+
+    @Test
+    public void getValue16x16() {
+        GrilleImpl test = new GrilleImpl(16);
+        assertEquals('0',test.getValue(9,5));
+    }
+
+    @Test
+    public void complete16x16() {
+        GrilleImpl test = new GrilleImpl(16);
+        assertEquals('0',test.getValue(9,5));
+    }
 }
