3034be30a49995306a5d5a89f0e30567b6dc811e
Fenouil <63717793+LeFenouil@users.noreply.github.com>
2022-05-19 21:07:13 +0200

    YO

--- src/main/java/GrilleImpl.java
+++ src/main/java/GrilleImpl.java
@@ -1,31 +1,61 @@
-/**
- * Info sur package-info.java
- */
-
 import java.util.Arrays;
 
 /**
- * Implementation d'une grille.
+ * Implementation d'une grille
  */
 public class GrilleImpl implements Grille {
 
     /**
-     * Variable de dimension.
+     * Variable de dimension
      */
     private int dim;
 
     /**
-     * Variable de dimension.
+     * Variable de dimension
      */
     private char[][] grille;
 
     /**
-     * Constructeur.
-     * @param dim correspond aux dimensions de la grille
+     * Constructeur
      */
     public GrilleImpl(int dim) {
         this.dim = dim;
         this.grille = new char[dim][dim];
+    }
+
+    /**
+     * Initialise le tab donne en parametre avec des valeurs EMPTY
+     */
+    public void initialisation(){
+        for (int i = 0; i < this.grille.length; i++) {
+            for (int j = 0; j < this.grille[i].length; j++) {
+                this.grille[i][j] = EMPTY;
+            }
+        }
+    }
+
+    /**
+     * @throws IllegalArgumentException si x ou y sont hors bornes (0 - dimension-1)
+     */
+    public void checkBornes(int x, int y){
+        if ( x > dim-1 || x < 0 || y > dim-1 || y < 0 ) {
+            throw new IllegalArgumentException("Hors borne.");
+        }
+    }
+
+    /**
+     * @throws IllegalArgumentException si char n'est pas compris dans la table des Possibles
+     */
+    public void checkPossibles(char value){
+        boolean ok = false;
+        for (int i = 0; i < Possible.length; i++) {
+            if (value == Possible[i]){
+                ok = true;
+            }
+        }
+        if (ok == false){
+            throw new IllegalArgumentException("Caractere pas autorise");
+        }
     }
 
     @Override
@@ -33,30 +63,20 @@ public class GrilleImpl implements Grille {
         return this.dim;
     }
 
-    /**
-     * @throws IllegalArgumentException si x ou y sont hors bornes (0 - dimension-1)
-     * @param x position du point x
-     * @param y position du point y
-     */
-    public void checkBornes(int x, int y){
-        if (x > dim-1 || x < 0 || y > dim-1 || y < 0) {
-            throw new IllegalArgumentException("Hors borne.");
-        }
-    }
-    
     @Override
     public void setValue(int x, int y, char value) throws IllegalArgumentException {
-	    checkBornes(x, y);
-        if (possible(x, y, value)) {
+	    checkBornes(x,y);
+
+	    if (possible(x, y, value)) {
             this.grille[x][y] = value;
         } else {
-            throw new IllegalArgumentException("C'est une valeur impossible");
+            throw new IllegalArgumentException("Valeur impossible");
         }
     }
 
     @Override
     public char getValue(int x, int y) throws IllegalArgumentException {
-	checkBornes(x, y);
+	    checkBornes(x,y);
         return this.grille[x][y];
     }
 
@@ -64,7 +84,7 @@ public class GrilleImpl implements Grille {
     public boolean complete() {
         for (int i = 0; i < this.grille.length; i++) {
             for (int j = 0; j < this.grille[i].length; j++) {
-                if (this.grille[i][j] == '@') {
+                if (this.grille[i][j] == EMPTY) {
                     return false;
                 }
             }
@@ -72,27 +92,15 @@ public class GrilleImpl implements Grille {
         return true;
     }
 
-
     @Override
     public boolean possible(int x, int y, char value) throws IllegalArgumentException {
-        boolean ok = false;
-	checkBornes(x, y);
 
-        for (int i = 0; i < Possible.length; i++) {
-            if (value == Possible[i]){
-                ok = true;
-            }
-        }
+	    checkBornes(x,y);
+        checkPossibles(value);
 
-        if (ok == false){
-            throw new IllegalArgumentException("Il ne s'agit pas d'un caractere autorise");
-        } else {
-            for (int i = 0; i < this.grille.length; i++) {
-                for (int j = 0; j < this.grille[i].length; j++) {
-                    if (this.grille[i][j] == value) {
-                        return false;
-                    }
-                }
+        for (int i = 0; i < this.grille.length; i++) {
+            if (this.grille[i][y] == value || this.grille[x][i] == value) {
+                return false;
             }
         }
         return true;
