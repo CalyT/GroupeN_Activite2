d5287fffcd22221879a3725598be0e50b2f60eab
Fenouil <63717793+LeFenouil@users.noreply.github.com>
2022-05-19 14:08:26 +0200

    Update GrilleImpl.java

--- src/main/java/GrilleImpl.java
+++ src/main/java/GrilleImpl.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Implementation d'une grille
  */
@@ -19,26 +21,42 @@ public class GrilleImpl implements Grille {
     public GrilleImpl(int dim) {
         this.dim = dim;
         this.grille = new char[dim][dim];
+        Arrays.fill(this.grille, '@');
     }
 
     @Override
     public int getDimension() {
-        return 0;
+        return this.dim;
     }
 
     @Override
     public void setValue(int x, int y, char value) throws IllegalArgumentException {
-
+        if (x > dim && x >= 0 || y > dim && y >= 0) {
+            throw new IllegalArgumentException("La case n'existe pas");
+        } else {
+            if (possible(x, y, value)) {
+                this.grille[x][y] = value;
+            } else {
+                throw new IllegalArgumentException("C'est une valeur impossible");
+            }
+        }
     }
 
     @Override
     public char getValue(int x, int y) throws IllegalArgumentException {
-        return '0';
+        return this.grille[x][y];
     }
 
     @Override
     public boolean complete() {
-        return false;
+        for (int i = 0; i < this.grille.length; i++) {
+            for (int j = 0; j < this.grille[i].length; i++) {
+                if (this.grille[i][j] == '@') {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
