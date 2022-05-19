1eb06ca1c09ced514084072b17260349ad53480b
Fenouil <63717793+LeFenouil@users.noreply.github.com>
2022-05-19 15:24:30 +0200

    Update GrilleImpl.java

--- src/main/java/GrilleImpl.java
+++ src/main/java/GrilleImpl.java
@@ -31,7 +31,7 @@ public class GrilleImpl implements Grille {
     @Override
     public void setValue(int x, int y, char value) throws IllegalArgumentException {
         if (x > dim && x >= 0 || y > dim && y >= 0) {
-            throw new IllegalArgumentException("La case n'existe pas");
+            throw new IllegalArgumentException("Hors borne.");
         } else {
             if (possible(x, y, value)) {
                 this.grille[x][y] = value;
@@ -43,6 +43,9 @@ public class GrilleImpl implements Grille {
 
     @Override
     public char getValue(int x, int y) throws IllegalArgumentException {
+        if (x > dim && x >= 0 || y > dim && y >= 0) {
+            throw new IllegalArgumentException("Hors borne.");
+        }
         return this.grille[x][y];
     }
 
@@ -58,22 +61,12 @@ public class GrilleImpl implements Grille {
         return true;
     }
 
-    /**
-     * Test si une valeur est possible dans la grille par rapport a ce qu'elle
-     * contient deja
-     *
-     * @param x     position x dans la grille
-     * @param y     position y dans la grille
-     * @param value valeur a mettre dans la case
-     * @throw IllegalArgumentException si x ou y sont hors bornes (0-8)
-     * @throw IllegalArgumentException si value n'est pas un caractere autorise
-     *        ('1',...,'9',..)
-     */
+
     @Override
     public boolean possible(int x, int y, char value) throws IllegalArgumentException {
         boolean ok = false;
         if (x > dim && x >= 0 || y > dim && y >= 0) {
-            throw new IllegalArgumentException("La case n'existe pas.");
+            throw new IllegalArgumentException("Hors borne.");
         }
         for (int i = 0; i < possible.length; i++) {
             if (value == possible[i]){
